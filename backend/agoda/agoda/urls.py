21a13fa9f2d608c54590d873da2f2772af061c16
tayfunnk96 <tayfun.kendiroglu20@gmail.com>
2024-01-11 17:17:31 +0300

    düzenleme

--- backend/agoda/agoda/urls.py
+++ backend/agoda/agoda/urls.py
@@ -27,12 +27,6 @@ urlpatterns = [
     path("login/",login,name="login"),
     path("register/",register,name="register"),
     path("endyear/", endyearPage, name="endyear"),
-    path("eliteoffers/", eliteoffersPage, name="eliteoffers"),
-    path("payless/", paylessPage, name="payless"),
-    path("staylonger/", staylongerPage, name="staylonger"),
-    path("deals/", dealsPage, name="deals"),
-    path("reservation/", reservationPage, name="reservation"),
-    path("chance/", chancePage, name="chance"),
     path("detay/<Cardid>",detail,name="detay"),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
